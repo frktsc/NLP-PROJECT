a734d5c18f89fe3147ace229e328d59f19c5de05
frktsc <ftasc40@gmail.com>
2024-03-27 03:47:43 -0400

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # NLP-PROJECT
+
+
+## how to run?
+
+```bash
+conda create -n hate python=3.8 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python app.py
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="NLP-PROJECT",
+    version="0.0.1",
+    author="frktsc",
+    author_email="ftasc40@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
